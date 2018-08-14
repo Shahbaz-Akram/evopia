275ce52cf839dfae648812d153f160b1c1f5ad14
Mian Shahbaz Akram <shahbazakram2@yahoo.com>
2018-08-14 21:17:35 +0500

    pod files

---
+++ Pods/Target Support Files/Pods-evopia/Pods-evopia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_evopia : NSObject
+@end
+@implementation PodsDummy_Pods_evopia
+@end
---
+++ evopia/LeftMenu/leftMenuViewController.swift
@@ -0,0 +1,24 @@
+//
+//  leftMenuViewController.swift
+//  evopia
+//
+//  Created by Shahbaz Akram on 8/14/18.
+//  Copyright © 2018 Shahbaz Akram. All rights reserved.
+//
+
+import UIKit
+
+class leftMenuViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: leftTableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
+
